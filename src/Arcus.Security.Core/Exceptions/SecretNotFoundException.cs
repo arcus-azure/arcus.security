3372f502add45deee12cdfa5c6c27b9ce8297b17
Sam Vanhoutte <Sam.Vanhoutte@codit.eu>
2018-12-17 11:48:39 +0100

    added inlinedoc and create secretnotfoundexception

---
+++ src/Arcus.Security.Core/Exceptions/SecretNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arcus.Security.Core.Exceptions
+{
+    /// <summary>
+    /// Exception, thrown when no secret was found, using the given name.
+    /// </summary>
+    public class SecretNotFoundException : Exception
+    {
+        public string Name { get;  }
+        public SecretNotFoundException(string name) : base($"The secret {name} was not found.")
+        {
+            Name = name;
+        }
+    }
+}
--- src/Arcus.Security.Core/ISecretProvider.cs
+++ src/Arcus.Security.Core/ISecretProvider.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Arcus.Security.Core.Exceptions;
 
 namespace Arcus.Security.Core
 {
+    /// <summary>
+    /// <see cref="ISecretProvider"/> allows developers to build specific Secret key providers.
+    /// </summary>
     public interface ISecretProvider
     {
+        /// <summary>
+        /// Retrieves the secret value, based on the given name
+        /// </summary>
+        /// <param name="name">The name of the secret key</param>
+        /// <returns>Returns a <see cref="Task{string}"/> that contains the secret key</returns>
+        /// <exception cref="ArgumentException">The name must not be empty</exception>
+        /// <exception cref="ArgumentNullException">The name must not be null</exception>
+        /// <exception cref="SecretNotFoundException">The secret was not found, using the given name</exception>
         Task<string> GetAsync(string name);
     }
 }
