1e92b4e7c55359d98c4b14fb9e6b13314b7e4d9e
Sam Vanhoutte <Sam.Vanhoutte@codit.eu>
2018-12-17 13:16:29 +0100

    initial keyvault project

--- src/Arcus.Security.KeyVault/KeyVaultSecretProvider.cs
+++ src/Arcus.Security.KeyVault/KeyVaultSecretProvider.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using Arcus.Security.Core;
+using Arcus.Security.Core.Interfaces;
 
 namespace Arcus.Security.KeyVault
 {
     public class KeyVaultSecretProvider : ISecretProvider
     {
-        public async Task<string> GetAsync(string name)
+        public async Task<string> Get(string name)
         {
             throw new NotImplementedException();
         }
