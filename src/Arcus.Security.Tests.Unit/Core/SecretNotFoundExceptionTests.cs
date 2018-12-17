b75766abbf0113881e1082e42b7fe799fe2fd050
Sam Vanhoutte <SamVanhoutte@users.noreply.github.com>
2018-12-17 12:32:20 +0100

    Arcus.Security.Core (#32)
    * Created Arcus.Security.Core project and Unit test project
    
    * added inlinedoc and create secretnotfoundexception
    
    * Package information for Arcus.Security.Core
    
    * enhancements code review (Guard for exception-keyname), serializable exception
    
    * docs for ctors of secretnotfoundexception

---
+++ src/Arcus.Security.Tests.Unit/Core/SecretNotFoundExceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arcus.Security.Core.Exceptions;
+using Xunit;
+
+namespace Arcus.Security.Tests.Unit.Core
+{
+    public class SecretNotFoundExceptionTests
+    {
+        [Fact]
+        public void Exception_CreateWithoutName_ShouldFailWithargumentException()
+        {
+            // Arrange
+            string secretName = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new SecretNotFoundException(secretName));
+            Assert.Throws<ArgumentException>(() => new SecretNotFoundException(secretName, null));
+        }
+    }
+}
