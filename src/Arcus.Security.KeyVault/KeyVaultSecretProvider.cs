a97863b163e5244f7d74a218133e8f1c27307197
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-12-18 19:11:06 +0100

    KeyVault -> Key Vault

--- src/Arcus.Security.KeyVault/KeyVaultSecretProvider.cs
+++ src/Arcus.Security.KeyVault/KeyVaultSecretProvider.cs
@@ -23,10 +23,10 @@ namespace Arcus.Security.KeyVault
         public string VaultUri { get; }
 
         /// <summary>
-        /// Creates an Azure KeyVault Secret provider, connected to a specific Azure Key Vault
+        /// Creates an Azure Key Vault Secret provider, connected to a specific Azure Key Vault
         /// </summary>
-        /// <param name="keyVaultClientFactory">A <see cref="KeyVaultClientFactory"/> implementation that will be used to generate the KeyVaultClient</param>
-        /// <param name="keyVaultUri">The Uri of the Azure KeyVault you want to connect to.  <example>https://{vaultname}nebulus-iot.vault.azure.net/</example></param>
+        /// <param name="keyVaultClientFactory">A <see cref="KeyVaultClientFactory"/> implementation that will be used to generate the Key VaultClient</param>
+        /// <param name="keyVaultUri">The Uri of the Azure Key Vault you want to connect to.  <example>https://{vaultname}.vault.azure.net/</example></param>
         public KeyVaultSecretProvider(KeyVaultClientFactory keyVaultClientFactory, string keyVaultUri)
         {
             Guard.NotNullOrEmpty(keyVaultUri, nameof(keyVaultUri));
@@ -37,10 +37,10 @@ namespace Arcus.Security.KeyVault
         }
 
         /// <summary>
-        /// Gets the secret from KeyVault, using the right secret name
+        /// Gets the secret from Key Vault, using the right secret name
         /// </summary>
         /// <param name="name">The secret name</param>
-        /// <returns>The value, stored in KeyVault</returns>
+        /// <returns>The value, stored in Key Vault</returns>
         /// <exception cref="SecretNotFoundException">The secret was not found, using the given name</exception>
         public async Task<string> Get(string name)
         {
