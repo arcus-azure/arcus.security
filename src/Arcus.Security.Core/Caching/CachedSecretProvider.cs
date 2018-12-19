39ab19d3cab95baf7c135ead0bcf0aa9ba82eb86
Sam Vanhoutte <Sam.Vanhoutte@codit.eu>
2018-12-19 14:38:51 +0100

    CachedSecretProvider and related unit tests

---
+++ src/Arcus.Security.Core/Caching/CachedSecretProvider.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Arcus.Security.Core.Exceptions;
+using Arcus.Security.Core.Interfaces;
+using GuardNet;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Arcus.Security.Core.Caching
+{
+    /// <inheritdoc cref="ISecretProvider"/>
+    /// <summary>
+    /// A Secret Provider that will cache secrets in memory, to improve performance
+    /// </summary>
+    public class CachedSecretProvider : ISecretProvider
+    {
+        private readonly ISecretProvider _secretProvider;
+        private readonly IMemoryCache _memoryCache;
+        private readonly TimeSpan _cacheDuration;
+
+        /// <summary>
+        /// Creating a new CachedSecretProvider with all required information
+        /// </summary>
+        /// <param name="secretProvider">The internal <see cref="ISecretProvider"/> used to retrieve the actual Secret Value, when not cached</param>
+        /// <param name="cacheDuration">The <see cref="TimeSpan"/> for which a retrieved value should be cached</param>
+        /// <param name="memoryCache">A <see cref="IMemoryCache"/> implementation that can cache data in memory.</param>
+        /// <exception cref="ArgumentNullException">The secretProvider and memoryCache parameters must not be null</exception>
+        public CachedSecretProvider(ISecretProvider secretProvider, TimeSpan cacheDuration, IMemoryCache memoryCache)
+        {
+            Guard.NotNull(secretProvider, nameof(secretProvider));
+            Guard.NotNull(memoryCache, nameof(memoryCache));
+
+            _secretProvider = secretProvider;
+            _memoryCache = memoryCache;
+            _cacheDuration = cacheDuration;
+        }
+
+        /// <summary>
+        /// Retrieves the secret value from the cache (when available) or from the internal ISecretProvider
+        /// </summary>
+        /// <param name="name">The name of the secret value to be retrieved</param>
+        /// <returns>The secret value</returns>
+        /// <exception cref="SecretNotFoundException">The secret was not found, using the given name</exception>
+        public async Task<string> Get(string name)
+        {
+            string secretValue;
+            // Look for cache key.
+            if (!_memoryCache.TryGetValue(name, out secretValue))
+            {
+                // Key not in cache, so get data.
+                secretValue = await _secretProvider.Get(name);
+
+                // Set cache options.
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    // Keep in cache for this time, reset time if accessed.
+                    .SetSlidingExpiration(_cacheDuration);
+
+                // Save data in cache.
+                _memoryCache.Set(name, secretValue, cacheEntryOptions);
+            }
+
+            return secretValue;
+        }
+    }
+}
---
+++ src/Arcus.Security.Tests.Unit/Core/Stubs/TestSecretProviderStub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Arcus.Security.Core.Interfaces;
+
+namespace Arcus.Security.Tests.Unit.Core.Stubs
+{
+    public class TestSecretProviderStub : ISecretProvider
+    {
+        public string SecretValue { get; set; }
+
+        public TestSecretProviderStub(string secretValue)
+        {
+            SecretValue = secretValue;
+        }
+
+        public int CallsMadeSinceCreation { get; private set; }
+
+        public async Task<string> Get(string name)
+        {
+            ++CallsMadeSinceCreation;
+            return SecretValue;
+        }
+    }
+}
