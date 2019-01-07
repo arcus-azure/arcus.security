d904927b8ac8acc9ea11199468a9f63657efaa7f
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-01-07 19:34:28 +0100

    Make sure that uri is specified and fix exception type
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Arcus.Security.Providers.AzureKeyVault/Configuration/KeyVaultConfiguration.cs
+++ src/Arcus.Security.Providers.AzureKeyVault/Configuration/KeyVaultConfiguration.cs
@@ -16,10 +16,12 @@ namespace Arcus.Security.Providers.AzureKeyVault.Configuration
         ///     Constructor
         /// </summary>
         /// <param name="vaultUri">The Uri of the Azure Key Vault you want to connect to.</param>
-        /// <exception cref="ArgumentNullException">Exception thrown when the vault is not using https</exception>
+        /// <exception cref="UriFormatException">Exception thrown when the vault is not using https</exception>
+        /// <exception cref="ArgumentNullException">No vault uri was specified</exception>
         public KeyVaultConfiguration(Uri vaultUri)
         {
-            Guard.For<ArgumentException>(() => vaultUri.Scheme != Uri.UriSchemeHttps);
+            Guard.NotNull(vaultUri, nameof(vaultUri));
+            Guard.For<UriFormatException>(() => vaultUri.Scheme != Uri.UriSchemeHttps);
 
             VaultUri = vaultUri;
         }
