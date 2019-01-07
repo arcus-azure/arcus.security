506d33820ad77e856ea709a46cc4903d16e4bc76
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-01-07 16:09:14 +0100

    Introduce ICacheConfiguration (#47)
    * Introduce ICacheConfiguration
    
    * Improve XML docs

---
+++ src/Arcus.Security.Core/Caching/Configuration/CacheConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Arcus.Security.Core.Caching.Configuration.Interfaces;
+using GuardNet;
+
+namespace Arcus.Security.Core.Caching.Configuration
+{
+    public class CacheConfiguration : ICacheConfiguration
+    {
+        /// <summary>
+        ///     Duration for which an entry should be cached
+        /// </summary>
+        public TimeSpan Duration { get; }
+
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <param name="duration">Duration for which an entry should be cached</param>
+        /// <exception cref="ArgumentException">Exception thrown when default timespan is specified as cache duration</exception>
+        public CacheConfiguration(TimeSpan duration)
+        {
+            Guard.For<ArgumentException>(() => duration == default(TimeSpan));
+
+            Duration = duration;
+        }
+
+        /// <summary>
+        ///     Constructor with default cache entry of 5 minutes
+        /// </summary>
+        public CacheConfiguration()
+        {
+            Duration = TimeSpan.FromMinutes(5);
+        }
+    }
+}
--- src/Arcus.Security.Secrets.Core/Caching/SecretProviderCachingExtensions.cs
+++ src/Arcus.Security.Secrets.Core/Caching/SecretProviderCachingExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Arcus.Security.Core.Caching.Configuration;
 using Arcus.Security.Secrets.Core.Interfaces;
 using Microsoft.Extensions.Caching.Memory;
 
@@ -7,32 +8,47 @@ namespace Arcus.Security.Secrets.Core.Caching
     public static class SecretProviderCachingExtensions
     {
         /// <summary>
-        /// Creates an <see cref="ICachedSecretProvider"/> instance that will use InMemoryCache to get SecretValues from the passed secretProvider
+        ///     Creates an <see cref="ICachedSecretProvider" /> instance that will use InMemoryCache to get SecretValues from the
+        ///     passed secretProvider
         /// </summary>
-        /// <param name="secretProvider">An instantiated <see cref="ISecretProvider"/> that will only be called if the value is not cached</param>
+        /// <param name="secretProvider">
+        ///     An instantiated <see cref="ISecretProvider" /> that will only be called if the value is
+        ///     not cached
+        /// </param>
         /// <param name="cachingDuration">The duration to cache secrets in memory</param>
-        /// <param name="memoryCache">Optional <see cref="IMemoryCache"/> that can be used for caching.  Defaults to a <see cref="MemoryCache"/> instance</param>
+        /// <param name="memoryCache">
+        ///     Optional <see cref="IMemoryCache" /> that can be used for caching.  Defaults to a
+        ///     <see cref="MemoryCache" /> instance
+        /// </param>
         /// <returns>A secret provider that caches values</returns>
         public static ICachedSecretProvider WithCaching(this ISecretProvider secretProvider, TimeSpan cachingDuration, IMemoryCache memoryCache)
         {
-            return new CachedSecretProvider(secretProvider, cachingDuration, memoryCache);
+            return new CachedSecretProvider(secretProvider, new CacheConfiguration(cachingDuration), memoryCache);
         }
 
         /// <summary>
-        /// Creates an <see cref="ICachedSecretProvider"/> instance that will use InMemoryCache to get SecretValues from the passed secretProvider
+        ///     Creates an <see cref="ICachedSecretProvider" /> instance that will use InMemoryCache to get SecretValues from the
+        ///     passed secretProvider
         /// </summary>
-        /// <param name="secretProvider">An instantiated <see cref="ISecretProvider"/> that will only be called if the value is not cached</param>
+        /// <param name="secretProvider">
+        ///     An instantiated <see cref="ISecretProvider" /> that will only be called if the value is
+        ///     not cached
+        /// </param>
         /// <param name="cachingDuration">The duration to cache secrets in memory</param>
         /// <returns>A secret provider that caches values</returns>
         public static ICachedSecretProvider WithCaching(this ISecretProvider secretProvider, TimeSpan cachingDuration)
         {
-            return new CachedSecretProvider(secretProvider, cachingDuration);
+            return new CachedSecretProvider(secretProvider, new CacheConfiguration(cachingDuration));
         }
 
         /// <summary>
-        /// Creates an <see cref="ICachedSecretProvider"/> instance that will use InMemoryCache to get SecretValues from the passed secretProvider
+        ///     Creates an <see cref="ICachedSecretProvider" /> instance that will use InMemoryCache to get SecretValues from the
+        ///     passed secretProvider
         /// </summary>
-        /// <param name="secretProvider">An instantiated <see cref="ISecretProvider"/> that will only be called if the value is not cached</param>
+        /// <param name="secretProvider">
+        ///     An instantiated <see cref="ISecretProvider" /> that will only be called if the value is
+        ///     not cached
+        /// </param>
         /// <returns>A secret provider that caches values</returns>
         public static ICachedSecretProvider WithCaching(this ISecretProvider secretProvider)
         {
