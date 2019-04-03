2b4ba5a2645d2ae6ddee32824beffb519bef2757
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2019-04-03 14:34:53 +0200

    FEAT: add client-throttling for TooManyRequests Key Vault response (#52)
    * Update KeyVaultSecretProvider.cs
    
    * FEAT: add client-throttling for TooManyRequests Key Vault response
    
    When the Key Vault service limits exceeds, we need to throttle the request to make sure we missue the Key Vault resources. This throttling happes when the Key Vault response has a HTTP status code 'Too Many Requests' (429) in the form of an exponential back-off.
    
    * PR-SUG: rename 'getSecret' > 'secretOperation'

--- src/Arcus.Security.Secrets.AzureKeyVault/KeyVaultSecretProvider.cs
+++ src/Arcus.Security.Secrets.AzureKeyVault/KeyVaultSecretProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Arcus.Security.Providers.AzureKeyVault.Authentication.Interfaces;
 using Arcus.Security.Providers.AzureKeyVault.Configuration.Interfaces;
@@ -8,6 +9,7 @@ using Arcus.Security.Secrets.Core.Interfaces;
 using GuardNet;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.KeyVault.Models;
+using Polly;
 
 namespace Arcus.Security.Secrets.AzureKeyVault
 {
@@ -18,8 +20,11 @@ namespace Arcus.Security.Secrets.AzureKeyVault
     {
         private readonly IKeyVaultAuthenticator _authenticator;
         private readonly IKeyVaultConfiguration _vaultConfiguration;
+
         private KeyVaultClient _keyVaultClient;
-        
+
+        private static readonly SemaphoreSlim LockCreateKeyVaultClient = new SemaphoreSlim(initialCount: 1, maxCount: 1);
+
         /// <summary>
         /// Uri of the vault
         /// </summary>
@@ -38,6 +43,10 @@ namespace Arcus.Security.Secrets.AzureKeyVault
             VaultUri = $"{vaultConfiguration.VaultUri.Scheme}://{vaultConfiguration.VaultUri.Host}";
 
             _vaultConfiguration = vaultConfiguration;
+
+            /** TODO: if we only allow a single instance (KeyVaultClient) being created for this type,
+             *       why shouldn't we then take a dependency on the client instead on the factory?
+             *       The factory can still create the instance for us, but this provider may not be the right responsible to call this creation. */
             _authenticator = authenticator;
         }
 
@@ -47,13 +56,17 @@ namespace Arcus.Security.Secrets.AzureKeyVault
         /// <param name="secretName">The secret name</param>
         /// <returns>The value, stored in Key Vault</returns>
         /// <exception cref="SecretNotFoundException">The secret was not found, using the given name</exception>
+        /// <exception cref="KeyVaultErrorException">The call for a secret resulted in an invalid response</exception>
         public async Task<string> Get(string secretName)
         {
             Guard.NotNullOrEmpty(secretName, nameof(secretName));
             try
             {
-                var keyVaultClient = await GetClientAsync();
-                var secretBundle = await keyVaultClient.GetSecretAsync(VaultUri, secretName);
+                KeyVaultClient keyVaultClient = await GetClientAsync();
+                SecretBundle secretBundle =
+                    await ThrottleTooManyRequests(
+                        () => keyVaultClient.GetSecretAsync(VaultUri, secretName));
+                
                 return secretBundle?.Value;
             }
             catch (KeyVaultErrorException keyVaultErrorException)
@@ -69,12 +82,36 @@ namespace Arcus.Security.Secrets.AzureKeyVault
 
         private async Task<KeyVaultClient> GetClientAsync()
         {
-            if (_keyVaultClient == null)
-            {
-                _keyVaultClient = await _authenticator.Authenticate();
-            }
+            await LockCreateKeyVaultClient.WaitAsync();
 
-            return _keyVaultClient;
+            try
+            {
+                if (_keyVaultClient == null)
+                {
+                    // TODO: why is this factory not returning an interface (the 'Microsoft.Azure.KeyVault.IKeyVaultClient' interface)?
+                    _keyVaultClient = await _authenticator.Authenticate();
+                }
+
+                return _keyVaultClient;
+            }
+            finally
+            {
+                LockCreateKeyVaultClient.Release();
+            }
+        }
+
+        private static Task<SecretBundle> ThrottleTooManyRequests(Func<Task<SecretBundle>> secretOperation)
+        {
+            /** Client-side throttling using exponential back-off when Key Vault service limit exceeds:
+             * 1. Wait 1 second, retry request
+             * 2. If still throttled wait 2 seconds, retry request
+             * 3. If still throttled wait 4 seconds, retry request
+             * 4. If still throttled wait 8 seconds, retry request
+             * 5. If still throttled wait 16 seconds, retry request */
+
+            return Policy.Handle<KeyVaultErrorException>(ex => (int) ex.Response.StatusCode == 429)
+                         .WaitAndRetryAsync(5, attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt - 1)))
+                         .ExecuteAsync(secretOperation);
         }
     }
 }
