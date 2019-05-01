d4134835ea728670a0c54a256fa40b3743d22de5
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2019-05-01 10:18:18 +0200

    PR-FIX: make sure all implementations of 'IKeyVaultAuthentication' is used

--- src/Arcus.Security.Providers.AzureKeyVault/Authentication/ManagedServiceIdentityAuthenticator.cs
+++ src/Arcus.Security.Providers.AzureKeyVault/Authentication/ManagedServiceIdentityAuthenticator.cs
@@ -8,17 +8,17 @@ namespace Arcus.Security.Providers.AzureKeyVault.Authentication
     /// <summary>
     ///     Azure Key Vault authentication by using Azure Managed Service Identity
     /// </summary>
-    public class ManagedServiceIdentityAuthenticator : IKeyVaultAuthenticator
+    public class ManagedServiceIdentityAuthenticator : IKeyVaultAuthentication
     {
         /// <summary>
         /// Authenticates with Azure Key Vault
         /// </summary>
         /// <returns>A <see cref="KeyVaultClient"/> client to use for interaction with the vault</returns>
-        public Task<KeyVaultClient> Authenticate()
+        public Task<IKeyVaultClient> Authenticate()
         {
             var tokenProvider = new AzureServiceTokenProvider();
             var authenticationCallback = new KeyVaultClient.AuthenticationCallback(tokenProvider.KeyVaultTokenCallback);
-            var keyVaultClient = new KeyVaultClient(authenticationCallback);
+            IKeyVaultClient keyVaultClient = new KeyVaultClient(authenticationCallback);
 
             return Task.FromResult(keyVaultClient);
         }
