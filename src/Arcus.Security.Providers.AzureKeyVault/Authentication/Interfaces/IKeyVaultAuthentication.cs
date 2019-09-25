4a4414571415ea79b37e1abe90a9bf412426923a
Frederik Gheysels <frederik.gheysels@codit.eu>
2019-09-25 21:39:09 +0200

    Removed the obsolete Authenticate method from IKeyVaultAuthentication
    Next to removing the method from the interface definition, this method has also been removed from classes that implement the IKeyVaultAuthentication interface.

--- src/Arcus.Security.Providers.AzureKeyVault/Authentication/Interfaces/IKeyVaultAuthentication.cs
+++ src/Arcus.Security.Providers.AzureKeyVault/Authentication/Interfaces/IKeyVaultAuthentication.cs
@@ -1,21 +1,13 @@
-﻿using System;
+﻿using Microsoft.Azure.KeyVault;
 using System.Threading.Tasks;
-using Microsoft.Azure.KeyVault;
 
-namespace Arcus.Security.Providers.AzureKeyVault.Authentication.Interfaces 
+namespace Arcus.Security.Providers.AzureKeyVault.Authentication.Interfaces
 {
     /// <summary>
     ///     Authentication provider for Azure Key Vault
     /// </summary>
     public interface IKeyVaultAuthentication
     {
-        /// <summary>
-        ///     Authenticates with Azure Key Vault
-        /// </summary>
-        /// <returns>A <see cref="IKeyVaultClient" /> client to use for interaction with the vault</returns>
-        [Obsolete("Use the " + nameof(AuthenticateAsync) + " method instead")]
-        Task<IKeyVaultClient> Authenticate();
-
         /// <summary>
         ///     Authenticates with Azure Key Vault
         /// </summary>
