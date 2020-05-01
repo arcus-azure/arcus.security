f1e395765dfe2fb3052116314075b9db0fed034b
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-05-01 10:50:55 +0200

    Feature - add expiration date to KeyVault secret

--- src/Arcus.Security.Core/Secret.cs
+++ src/Arcus.Security.Core/Secret.cs
@@ -13,15 +13,17 @@ namespace Arcus.Security.Core
         /// </summary>
         /// <param name="value">The secret value.</param>
         /// <param name="version">The version of the secret.</param>
+        /// <param name="expirationDate">The expiration date of the secret.</param>
         /// <exception cref="ArgumentNullException">The <paramref name="value"/> cannot be <c>null</c>.</exception>
         /// <exception cref="ArgumentNullException">The <paramref name="version"/> cannot be <c>null</c>.</exception>
-        public Secret(string value, string version)
+        public Secret(string value, string version, DateTime? expirationDate = null)
         {
             Guard.NotNull(value, nameof(value));
             Guard.NotNull(version, nameof(version));
 
             Value = value;
             Version = version;
+            Expires = expirationDate;
         }
 
         /// <summary>
@@ -34,5 +36,10 @@ namespace Arcus.Security.Core
         /// </summary>
         /// <remarks>Version is not checked for <c>null</c>.</remarks>
         public string Version { get; }
+
+        /// <summary>
+        /// Gets the optional expiration date of the secret.
+        /// </summary>
+        public DateTime? Expires { get; }
     }
 }
