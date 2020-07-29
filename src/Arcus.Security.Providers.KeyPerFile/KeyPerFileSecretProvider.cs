26f5e1c590049a8f29331f45c9fef9e4e9ee298c
Frederik Gheysels <frederik.gheysels@codit.eu>
2020-07-29 22:21:17 +0200

    Add KeyPerFileSecretProvider

---
+++ src/Arcus.Security.Providers.KeyPerFile/KeyPerFileSecretProvider.cs
@@ -0,0 +1,59 @@
+﻿using Arcus.Security.Core;
+using GuardNet;
+using Microsoft.Extensions.Configuration.KeyPerFile;
+using System;
+using System.Threading.Tasks;
+
+namespace Arcus.Security.Providers.KeyPerFile
+{
+    public class KeyPerFileSecretProvider : ISecretProvider
+    {
+        private readonly KeyPerFileConfigurationProvider _provider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyPerFileSecretProvider"/> class.
+        /// </summary>
+        public KeyPerFileSecretProvider(KeyPerFileConfigurationSource configurationSource)
+        {
+            Guard.NotNull(configurationSource, nameof(configurationSource));
+            _provider = new KeyPerFileConfigurationProvider(configurationSource);
+        }
+
+        /// <summary>
+        /// Retrieves the secret value, based on the given name
+        /// </summary>
+        /// <param name="secretName">The name of the secret key</param>
+        /// <returns>Returns the secret key.</returns>
+        /// <exception cref="ArgumentException">The <paramref name="secretName"/> must not be empty</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="secretName"/> must not be null</exception>
+        /// <exception cref="SecretNotFoundException">The secret was not found, using the given name</exception>
+        public Task<string> GetRawSecretAsync(string secretName)
+        {
+            if (_provider.TryGet(secretName, out string value))
+            {
+                return Task.FromResult(value);
+            }
+
+            return Task.FromResult<string>(null);
+        }
+
+        /// <summary>
+        /// Retrieves the secret value, based on the given name
+        /// </summary>
+        /// <param name="secretName">The name of the secret key</param>
+        /// <returns>Returns a <see cref="Secret"/> that contains the secret key</returns>
+        /// <exception cref="ArgumentException">The <paramref name="secretName"/> must not be empty</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="secretName"/> must not be null</exception>
+        /// <exception cref="SecretNotFoundException">The secret was not found, using the given name</exception>
+        public async Task<Secret> GetSecretAsync(string secretName)
+        {
+            string secretValue = await GetRawSecretAsync(secretName);
+            if (secretValue == null)
+            {
+                return null;
+            }
+
+            return new Secret(secretValue);
+        }
+    }
+}
---
+++ src/Arcus.Security.Providers.KeyPerFile/SecretStoreBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration.KeyPerFile;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace Arcus.Security.Providers.KeyPerFile
+{
+    public static class SecretStoreBuilderExtensions
+    {
+        public static SecretStoreBuilder AddKeyPerFile(this SecretStoreBuilder builder, string directoryPath, bool optional = true)
+        {
+            KeyPerFileConfigurationSource configuration = new KeyPerFileConfigurationSource();
+
+            configuration.FileProvider = new PhysicalFileProvider(directoryPath);
+            configuration.Optional = optional;
+
+            return builder.AddProvider(new KeyPerFileSecretProvider(configuration));
+        }
+    }
+}
