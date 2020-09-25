7f41fda9dbbd288e62634c398a2f7e7502a03ece
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-09-25 07:45:47 +0200

    pr-sug: add more thorough critical exception filter unit tests

--- src/Arcus.Security.Tests.Unit/Core/CriticalExceptionFilterTests.cs
+++ src/Arcus.Security.Tests.Unit/Core/CriticalExceptionFilterTests.cs
@@ -15,22 +15,46 @@ namespace Arcus.Security.Tests.Unit.Core
         {
             // Arrange
             var statusCode = HttpStatusCode.Unauthorized;
+            
+            // Act
             var filter = new CriticalExceptionFilter(
                 typeof(HttpOperationException),
                 ex => ex is HttpOperationException httpException
                       && httpException.Response.StatusCode == statusCode);
 
-            var response = new HttpResponseMessage(statusCode);
-            var exception = new HttpOperationException("Some HTTP failure")
+            // Assert
+            var expectedException = new HttpOperationException("Some HTTP failure")
             {
-                Response = new HttpResponseMessageWrapper(response, "Some ignored response content")
+                Response = new HttpResponseMessageWrapper(
+                    new HttpResponseMessage(statusCode), 
+                    "Some ignored response content")
             };
 
+            var notExpectedException = new HttpOperationException("Som other HTTP failure")
+            {
+                Response = new HttpResponseMessageWrapper(
+                    new HttpResponseMessage(HttpStatusCode.BadGateway), 
+                    "Some ignored response content")
+            };
+
+            Assert.True(filter.IsCritical(expectedException));
+            Assert.False(filter.IsCritical(notExpectedException));
+            Assert.False(filter.IsCritical(new AuthenticationException()));
+            Assert.Equal(typeof(HttpOperationException), filter.ExceptionType);
+        }
+
+        [Fact]
+        public void IsCritical_WithExceptionType_Succeeds()
+        {
             // Act
-            bool isCritical = filter.IsCritical(exception);
+            var filter = new CriticalExceptionFilter(
+                typeof(AuthenticationException), 
+                exception => exception is AuthenticationException);
 
             // Assert
-            Assert.True(isCritical);
+            Assert.True(filter.IsCritical(new AuthenticationException()));
+            Assert.False(filter.IsCritical(new AccessViolationException()));
+            Assert.Equal(typeof(AuthenticationException), filter.ExceptionType);
         }
         
         [Fact]
