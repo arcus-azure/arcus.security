c2c57f781c0573a5c0883cc5216d184e3c37164e
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-11-13 07:01:52 +0100

    pr-sug: write dependency tracking to Application Insights

--- src/Arcus.Security.Tests.Integration/IntegrationTest.cs
+++ src/Arcus.Security.Tests.Integration/IntegrationTest.cs
@@ -1,21 +1,41 @@
-﻿using Arcus.Security.Tests.Integration.Fixture;
+﻿using System;
+using Arcus.Security.Tests.Integration.Fixture;
 using Arcus.Testing.Logging;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Extensions.Logging;
 using Xunit.Abstractions;
 
 namespace Arcus.Security.Tests.Integration
 {
-    public class IntegrationTest
+    public class IntegrationTest : IDisposable
     {
+        private readonly ILoggerFactory _loggerFactory;
+
         protected TestConfig Configuration { get; }
-        protected XunitTestLogger Logger { get; }
+        protected Microsoft.Extensions.Logging.ILogger Logger { get; }
 
         public IntegrationTest(ITestOutputHelper testOutput)
         {
-            Logger = new XunitTestLogger(testOutput);
-
             // The appsettings.local.json allows users to override (gitignored) settings locally for testing purposes
             Configuration = TestConfig.Create();
+            
+            var configuration = new LoggerConfiguration()
+                .WriteTo.Sink(new XunitTestLogSink(testOutput))
+                .WriteTo.AzureApplicationInsights(Configuration.GetValue<string>("Arcus:ApplicationInsights:InstrumentationKey"));
+
+            _loggerFactory = new SerilogLoggerFactory(configuration.CreateLogger(), dispose: true);
+            Logger = _loggerFactory.CreateLogger(nameof(IntegrationTest));
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            _loggerFactory.Dispose();
         }
     }
 }
