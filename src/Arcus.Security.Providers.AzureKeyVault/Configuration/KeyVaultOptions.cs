fae34d8d98595487595313306e19532c7276b170
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-11-18 07:50:15 +0100

    Feature - track Azure Key Vault dependency (#207)
    * Feature - track Azure Key Vault dependency
    
    * pr-add: key vault options to opt-in
    
    * pr-add: tracking dependency test
    
    * pr-docs: update feature docs for Azure Key Vault
    
    * pr-fix: cache configuration in correct argument position
    
    * pr-sug: update with correct dependency assertion
    
    * pr-fix: update with passing allong the configure options function
    
    * pr-fix: pass along the configureOptions function
    
    * pr-sug: provide alternative tracking of dependency
    
    * pr-sug: write dependency tracking to Application Insights
    
    * pr-fix: use Serilog for integration test loggign
    
    * pr-fix: dispose loggers
    
    * pr-fix: use Serilog loggers
    
    * pr-fix: dispose host
    
    * pr-fix: use correct Serilog assertion
    
    * pr-sug: simplify the assertion
    
    * Update key-vault.md

---
+++ src/Arcus.Security.Providers.AzureKeyVault/Configuration/KeyVaultOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Arcus.Security.Providers.AzureKeyVault.Configuration
+{
+    /// <summary>
+    /// Represents the available options to configure extra options of the Azure Key Vault for the <see cref="KeyVaultSecretProvider"/>.
+    /// </summary>
+    public class KeyVaultOptions
+    {
+        /// <summary>
+        /// Gets or sets the flag to indicate whether or not the <see cref="KeyVaultSecretProvider"/> should track the Azure Key Vault dependency.
+        /// </summary>
+        public bool TrackDependency { get; set; } = false;
+    }
+}
--- src/Arcus.Security.Tests.Integration/IntegrationTest.cs
+++ src/Arcus.Security.Tests.Integration/IntegrationTest.cs
@@ -1,21 +1,43 @@
-﻿using Arcus.Security.Tests.Integration.Fixture;
+﻿using System;
+using Arcus.Security.Tests.Core.Stubs;
+using Arcus.Security.Tests.Integration.Fixture;
 using Arcus.Testing.Logging;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Extensions.Logging;
 using Xunit.Abstractions;
 
 namespace Arcus.Security.Tests.Integration
 {
-    public class IntegrationTest
+    public class IntegrationTest : IDisposable
     {
         protected TestConfig Configuration { get; }
-        protected XunitTestLogger Logger { get; }
+        protected Logger Logger { get; }
+        protected InMemoryLogSink InMemoryLogSink { get; }
 
         public IntegrationTest(ITestOutputHelper testOutput)
         {
-            Logger = new XunitTestLogger(testOutput);
-
             // The appsettings.local.json allows users to override (gitignored) settings locally for testing purposes
             Configuration = TestConfig.Create();
+            InMemoryLogSink = new InMemoryLogSink();
+            
+            var configuration = new LoggerConfiguration()
+                .WriteTo.Sink(new XunitTestLogSink(testOutput))
+                .WriteTo.Sink(InMemoryLogSink)
+                .WriteTo.AzureApplicationInsights(Configuration.GetValue<string>("Arcus:ApplicationInsights:InstrumentationKey"));
+
+            Logger = configuration.CreateLogger();
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Logger.Dispose();
         }
     }
 }
