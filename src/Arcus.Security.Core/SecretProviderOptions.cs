d325f6a481c02b4bdaf230c43c8558851348a498
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-11-27 05:58:56 +0100

    Update SecretProviderOptions.cs

--- src/Arcus.Security.Core/SecretProviderOptions.cs
+++ src/Arcus.Security.Core/SecretProviderOptions.cs
@@ -8,6 +8,8 @@ namespace Arcus.Security.Core
     /// </summary>
     public class SecretProviderOptions
     {
+        private readonly string _name;
+
         /// <summary>
         /// Gets or sets the function to mutate the secret name before looking it up.
         /// </summary>
@@ -17,6 +19,15 @@ namespace Arcus.Security.Core
         /// Gets or sets the name of the <see cref="ISecretProvider"/> to be registered in the secret store.
         /// </summary>
         /// <exception cref="ArgumentException">Thrown when the <paramref name="value"/> is blank.</exception>
-        public string Name { get; set; }
+        public string Name 
+        {  
+            get => _name;
+            set
+            {
+                 Guard.For<ArgumentException>(
+                     () => value != null && String.IsNullOrWhiteSpace(value),
+                     "Requires a non-blank value for the name of the secret provider to be registered in the secret store"););
+            }
+        }
     }
 }
