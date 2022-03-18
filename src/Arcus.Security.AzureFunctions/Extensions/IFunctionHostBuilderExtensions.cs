0e1b9e9ceae74d9d906b693699019a40441e9570
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2022-03-18 10:03:48 +0100

    feat: add config az func secret store extension (#333)
    * feat: add config az func secret store extension
    
    * pr-fix: use net6.0 az func
    
    * pr-fix: use correct docker file
    
    * pr-fix: use correct file paths in docker file
    
    * pr-fix: update with token replacement in app settings
    
    * pr-fix: correct az func routes
    
    * pr-add: az func docker test to release build

--- src/Arcus.Security.AzureFunctions/Extensions/IFunctionHostBuilderExtensions.cs
+++ src/Arcus.Security.AzureFunctions/Extensions/IFunctionHostBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Arcus.Security.Core;
 using GuardNet;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -25,6 +26,24 @@ namespace Microsoft.Azure.Functions.Extensions.DependencyInjection
             Guard.NotNull(configureSecretStores, nameof(configureSecretStores), "Requires a function to configure the secret store with potential secret providers");
 
             functionsHostBuilder.Services.AddSecretStore(configureSecretStores);
+            return functionsHostBuilder;
+        }
+
+        /// <summary>
+        /// Configure an <see cref="ISecretProvider"/> in the application with a given set of stores configured in the given <paramref name="configureSecretStores"/>.
+        /// </summary>
+        /// <param name="functionsHostBuilder">The builder to append the secret store configuration to.</param>
+        /// <param name="configureSecretStores">The customization of the different target secret store sources to include in the final <see cref="ISecretProvider"/>.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="functionsHostBuilder"/> or <paramref name="configureSecretStores"/> is <c>null</c>.</exception>
+        public static IFunctionsHostBuilder ConfigureSecretStore(
+            this IFunctionsHostBuilder functionsHostBuilder,
+            Action<FunctionsHostBuilderContext, IConfiguration, SecretStoreBuilder> configureSecretStores)
+        {
+            Guard.NotNull(functionsHostBuilder, nameof(functionsHostBuilder), "Requires a functions host builder to add the secret store");
+            Guard.NotNull(configureSecretStores, nameof(configureSecretStores), "Requires a function to configure the secret store with potential secret providers");
+
+            FunctionsHostBuilderContext context = functionsHostBuilder.GetContext();
+            functionsHostBuilder.Services.AddSecretStore(stores => configureSecretStores(context, context.Configuration, stores));
             return functionsHostBuilder;
         }
     }
