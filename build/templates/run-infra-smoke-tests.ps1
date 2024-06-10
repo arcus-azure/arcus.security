bb33679ed00efd49e444466e86d8ed707fd16817
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2024-06-10 09:11:06 +0200

    pr-fix: correct running

--- build/templates/run-infra-smoke-tests.ps1
+++ build/templates/run-infra-smoke-tests.ps1
@@ -6,16 +6,22 @@ param (
 
 Describe "key vault" {
   Context "availability" {
-    It "should contain a single vault with name" {
+    It "should contain only a single vault with name" {
       $vaults = Get-AzKeyVault -ResourceGroupName $resourceGroupName
       $vaults.Count | Should -Be 1
       $vaults[0].Name | Should -Be $keyVaultName
     }
     It "should contain an enabled secret with name" {
-      $secrets =$actualSecret | Should -Be $secretName
+      $secrets = $actualSecret | Should -Be $secretName
       $secrets.Name | Should -Contain $secretName 
       $secret = $secrets | Where-Object { $_.Name -eq $secretName } | Select-Object -First 1
       $secret.Enabled | Should -Be $true -Message "should be marked 'enabled'"
+    }
+  }
+  Context "configuration" {
+    It "should use SKU Standard" {
+      $vault = Get-AzKeyVault -ResourceGroupName $resourceGroupName -VaultName $keyVaultName
+      $vault.Sku | Should -Be "Standard"
     }
   }
 }
