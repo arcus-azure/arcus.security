2e824dee32cdb046e69247020bf28a66ab7f2722
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-09-26 18:01:23 +0200

    chore(core): internally implement core providers w/ new interface (#467)
    Internally implement the new `ISecretProvider` interface in the core
    providers and deprecate the current functionality.
    Relates to #448 
    Closes #454

--- src/Arcus.Security.Core/Extensions/SecretStoreBuilderExtensions.cs
+++ src/Arcus.Security.Core/Extensions/SecretStoreBuilderExtensions.cs
@@ -11,6 +11,61 @@ namespace Microsoft.Extensions.Hosting
     public static class SecretStoreBuilderExtensions
     {
         /// <summary>
+        /// Adds a secret source to the secret store of the application that gets its secrets from the environment (target: <see cref="EnvironmentVariableTarget.Process"/>).
+        /// </summary>
+        /// <param name="builder">The builder to create the secret store.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddEnvironmentVariables(this SecretStoreBuilder builder)
+        {
+            return AddEnvironmentVariables(builder, configureOptions: null);
+        }
+
+        /// <summary>
+        /// Adds a secret source to the secret store of the application that gets its secrets from the environment (target: <see cref="EnvironmentVariableTarget.Process"/>).
+        /// </summary>
+        /// <param name="builder">The builder to create the secret store.</param>
+        /// <param name="configureOptions">The optional function to manipulate the registration of the secret provider.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddEnvironmentVariables(this SecretStoreBuilder builder, Action<EnvironmentVariableSecretProviderOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+            return builder.AddProvider((_, _, options) => new EnvironmentVariableSecretProvider(options), configureOptions);
+        }
+
+        /// <summary>
+        /// Adds a secret source to the secret store of the application that gets its secrets from the <see cref="IConfiguration"/>.
+        /// </summary>
+        /// <param name="builder">The builder to create the secret store.</param>
+        /// <param name="configuration">The configuration of the application, containing secrets.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> or the <paramref name="configuration"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddConfiguration(this SecretStoreBuilder builder, IConfiguration configuration)
+        {
+            return AddConfiguration(builder, configuration, configureOptions: null);
+        }
+
+        /// <summary>
+        /// Adds a secret source to the secret store of the application that gets its secrets from the <see cref="IConfiguration"/>.
+        /// </summary>
+        /// <param name="builder">The builder to create the secret store.</param>
+        /// <param name="configuration">The configuration of the application, containing secrets.</param>
+        /// <param name="configureOptions">The optional function to manipulate the registration of the secret provider.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> or the <paramref name="configuration"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddConfiguration(
+            this SecretStoreBuilder builder,
+            IConfiguration configuration,
+            Action<SecretProviderRegistrationOptions> configureOptions)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+            return builder.AddProvider(new ConfigurationSecretProvider(configuration), configureOptions);
+        }
+    }
+
+    /// <summary>
+    /// Extends the <see cref="SecretStoreBuilder"/> to provide additional secret sources.
+    /// </summary>
+    public static class DeprecatedSecretStoreBuilderExtensions
+    {
+        /// <summary>
         /// Adds a secret source to the secret store of the application that gets its secrets from the environment.
         /// </summary>
         /// <param name="builder">The builder to create the secret store.</param>
@@ -19,6 +74,7 @@ namespace Microsoft.Extensions.Hosting
         /// <param name="mutateSecretName">The optional function to mutate the secret name before looking it up.</param>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
         /// <exception cref="ArgumentException">Thrown when the <paramref name="target"/> is outside the bounds of the enumeration.</exception>
+        [Obsolete("Will be removed in v3.0 in favor of configuring the secret provider registration with options")]
         public static SecretStoreBuilder AddEnvironmentVariables(
             this SecretStoreBuilder builder,
             EnvironmentVariableTarget target = EnvironmentVariableSecretProvider.DefaultTarget,
@@ -39,6 +95,7 @@ namespace Microsoft.Extensions.Hosting
         /// <param name="mutateSecretName">The optional function to mutate the secret name before looking it up.</param>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
         /// <exception cref="ArgumentException">Thrown when the <paramref name="target"/> is outside the bounds of the enumeration.</exception>
+        [Obsolete("Will be removed in v3.0 in favor of configuring the secret provider registration with options")]
         public static SecretStoreBuilder AddEnvironmentVariables(
             this SecretStoreBuilder builder,
             EnvironmentVariableTarget target,
@@ -46,20 +103,28 @@ namespace Microsoft.Extensions.Hosting
             string name,
             Func<string, string> mutateSecretName)
         {
-            if (builder is null)
-            {
-                throw new ArgumentNullException(nameof(builder), "Requires a secret store builder to add the environment secrets");
-            }
-
             if (!Enum.IsDefined(typeof(EnvironmentVariableTarget), target))
             {
                 throw new ArgumentException($"Requires an environment variable target of either '{EnvironmentVariableTarget.Process}', '{EnvironmentVariableTarget.Machine}', or '{EnvironmentVariableTarget.User}'");
             }
 
-            return builder.AddProvider(new EnvironmentVariableSecretProvider(target, prefix), options =>
+            return builder.AddEnvironmentVariables(options =>
             {
-                options.Name = name;
-                options.MutateSecretName = mutateSecretName;
+                options.Target = target;
+                if (!string.IsNullOrWhiteSpace(prefix))
+                {
+                    options.Prefix = prefix;
+                }
+
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    options.ProviderName = name;
+                }
+
+                if (mutateSecretName != null)
+                {
+                    options.MapSecretName(mutateSecretName);
+                }
             });
         }
 
@@ -70,6 +135,7 @@ namespace Microsoft.Extensions.Hosting
         /// <param name="configuration">The configuration of the application, containing secrets.</param>
         /// <param name="mutateSecretName">The function to mutate the secret name before looking it up.</param>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        [Obsolete("Will be removed in v3.0 in favor of configuring the secret provider registration with options")]
         public static SecretStoreBuilder AddConfiguration(
             this SecretStoreBuilder builder,
             IConfiguration configuration,
@@ -87,26 +153,24 @@ namespace Microsoft.Extensions.Hosting
         /// <param name="name">The unique name to register this Configuration provider in the secret store.</param>
         /// <param name="mutateSecretName">The optional function to mutate the secret name before looking it up.</param>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        [Obsolete("Will be removed in v3.0 in favor of configuring the secret provider registration with options")]
         public static SecretStoreBuilder AddConfiguration(
             this SecretStoreBuilder builder,
             IConfiguration configuration,
             string name,
             Func<string, string> mutateSecretName)
         {
-            if (builder is null)
+            return builder.AddConfiguration(configuration, options =>
             {
-                throw new ArgumentNullException(nameof(builder), "Requires a secret store builder to add the configuration secrets");
-            }
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    options.ProviderName = name;
+                }
 
-            if (configuration is null)
-            {
-                throw new ArgumentNullException(nameof(configuration), "Requires a configuration instance to retrieve the secrets from");
-            }
-
-            return builder.AddProvider(new ConfigurationSecretProvider(configuration), options =>
-            {
-                options.Name = name;
-                options.MutateSecretName = mutateSecretName;
+                if (mutateSecretName != null)
+                {
+                    options.MapSecretName(mutateSecretName);
+                }
             });
         }
     }
