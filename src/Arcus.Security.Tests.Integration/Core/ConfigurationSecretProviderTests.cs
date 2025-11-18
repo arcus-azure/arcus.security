25359748c833aa8b82f60f397c5bbbb32d4af5ab
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-11-18 08:32:10 +0100

    test(core): add test-contexts tests for core secret providers

---
+++ src/Arcus.Security.Tests.Integration/Core/ConfigurationSecretProviderTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Arcus.Security.Core.Providers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Arcus.Security.Tests.Integration.Core
+{
+    public class ConfigurationSecretProviderTests : IntegrationTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationSecretProviderTests"/> class.
+        /// </summary>
+        public ConfigurationSecretProviderTests(ITestOutputHelper outputWriter) : base(outputWriter)
+        {
+        }
+
+        [Fact]
+        public async Task GetSecret_WithAvailableApplicationConfigurationValue_SucceedsByFindingSecret()
+        {
+            // Arrange
+            var configSecret = GivenNewAppConfigSecret();
+
+            await using var store = GivenSecretStore(
+                host => host.ConfigureAppConfiguration((_, config) =>
+                {
+                    config.AddInMemoryCollection([configSecret]);
+                }),
+                (config, store) =>
+                {
+                    // Act
+                    WhenConfigurationFor(store, config);
+                });
+
+            // Assert
+            store.ShouldFindProvider<ConfigurationSecretProvider>();
+            await store.ShouldFindSecretAsync(configSecret.Key, configSecret.Value);
+        }
+
+        private static KeyValuePair<string, string> GivenNewAppConfigSecret()
+        {
+            return new KeyValuePair<string, string>(Bogus.Random.Guid().ToString(), Bogus.Random.Guid().ToString());
+        }
+
+        private void WhenConfigurationFor(SecretStoreBuilder store, IConfiguration config)
+        {
+            store.AddConfiguration(config, ConfigureOptions);
+        }
+    }
+}
---
+++ src/Arcus.Security.Tests.Integration/Core/EnvironmentVariableSecretProviderTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Arcus.Security.Core.Providers;
+using Arcus.Testing;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Arcus.Security.Tests.Integration.Core
+{
+    public class EnvironmentVariableSecretProviderTests : IntegrationTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnvironmentVariableSecretProviderTests"/> class.
+        /// </summary>
+        public EnvironmentVariableSecretProviderTests(ITestOutputHelper outputWriter) : base(outputWriter)
+        {
+        }
+
+        [Fact]
+        public async Task GetSecret_WithAvailableEnvironmentVariable_SucceedsByFindingSecret()
+        {
+            // Arrange
+            string secretName = Bogus.Random.Guid().ToString("N").ToUpperInvariant();
+            string secretValue = Bogus.Random.Guid().ToString();
+            using var envVar = GivenEnvironmentVariable(secretName, secretValue);
+
+            await using var store = GivenSecretStore(store =>
+            {
+                // Act
+                store.AddEnvironmentVariables(ConfigureOptions);
+            });
+
+            // Assert
+            store.ShouldFindProvider<EnvironmentVariableSecretProvider>();
+            await store.ShouldFindSecretAsync(secretName, secretValue);
+        }
+
+        private TemporaryEnvironmentVariable GivenEnvironmentVariable(string secretName, string secretValue)
+        {
+            return TemporaryEnvironmentVariable.SetSecretIfNotExists(secretName, secretValue, Logger);
+        }
+    }
+}
