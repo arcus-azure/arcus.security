c202515a5ffa0ff8f6bfe765efbc00999297edd4
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-11-18 10:13:41 +0100

    test(docker.secrets): add test context test for docker secrets secret provider

---
+++ src/Arcus.Security.Tests.Integration/DockerSecrets/Fixture/TemporaryDockerSecret.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Arcus.Security.Tests.Integration.DockerSecrets.Fixture
+{
+    internal class TemporaryDockerSecret : IDisposable
+    {
+        private readonly string _secretName;
+        private readonly ILogger _logger;
+
+        private TemporaryDockerSecret(string secretName, string secretLocation, ILogger logger)
+        {
+            _secretName = secretName;
+            _logger = logger;
+            Location = secretLocation;
+        }
+
+        public string Location { get; }
+
+        public static async Task<TemporaryDockerSecret> CreateNewAsync(string secretName, string secretValue, ILogger logger)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(secretName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(secretValue);
+
+            var secretLocation = Path.Combine(Path.GetTempPath(), "dockersecretstests");
+            Directory.CreateDirectory(secretLocation);
+
+            string secretPath = Path.Combine(secretLocation, secretName);
+            logger.LogDebug("[Test:Setup] add Docker secret '{SecretName}' with value '{SecretValue}", secretName, secretValue);
+            await File.WriteAllTextAsync(secretPath, secretValue);
+
+
+            return new TemporaryDockerSecret(secretName, secretLocation, logger);
+        }
+
+        public void Dispose()
+        {
+            _logger.LogDebug("[Test:Teardown] remove Docker secret '{SecretName}'", _secretName);
+            Directory.Delete(Location, true);
+        }
+    }
+}
