ade9966d55a2dc5f6c09bf2d18a97f693b82f8ea
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-11-26 06:39:39 +0100

    chore(deps): upgrade active dependencies throughout solution for .NET 10 release

---
+++ src/Arcus.Security.Tests.Unit/Core/Stubs/InMemoryLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.Extensions.Logging;
+
+namespace Arcus.Testing
+{
+    internal class InMemoryLogger : ILogger
+    {
+        private readonly Collection<string> _messages = [];
+        internal IReadOnlyCollection<string> Messages => _messages;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            _messages.Add(formatter(state, exception));
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable BeginScope<TState>(TState state) where TState : notnull
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
